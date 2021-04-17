b36da1a47b5bbcd60b69dcff9733cd0d4eb0c05e
Jamie Tardi <jamie.tardi@gmail.com>
2021-04-17 09:10:13 +0100

    setting up git authors

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Desktop = () => {
 	return (
-		<div className='desktop-main w-screen h-screen bg-center bg-cover'></div>
+		<div className='desktop-main w-screen h-screen bg-center bg-cover'>sdf</div>
 	);
 };
 
