40909c6725c6cc4fe290293a69b2b8193457e84e
Jamie Tardi <jamie.tardi@gmail.com>
2021-04-18 18:29:56 +0100

    adding clock functionality

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Clock = () => {
 	const current = new Date();
-	const hoursStr = current.getHours();
-	const minsStr = current.getMinutes();
+	const [hoursStr, setHourStr] = useState(current.getHours());
+	const [minsStr, setMinsStr] = useState(current.getMinutes());
+	const [secsStr, setSecsStr] = useState(current.getSeconds());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSecsStr((secsStr) => secsStr + 1);
+			setHourStr(current.getHours());
+			setMinsStr((minsStr) => (minsStr = current.getMinutes()));
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
 
 	return (
 		<div className='bg-clock h-8 w-auto flex justify-center items-center'>
 			<p className='text-white px-5'>
-				{hoursStr}:{minsStr}PM
+				{hoursStr}:{minsStr}
+				{/* {secsStr < 60 ? secsStr : setSecsStr(current.getSeconds())} */}
+				{hoursStr >= 12 ? 'pm' : 'am'}
 			</p>
 		</div>
 	);
